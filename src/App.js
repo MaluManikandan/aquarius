2f98c2c334921a81ad0b539cd6ab71583d0394ac
malumanikandan <malumanikandan.in@gmail.com>
2022-02-25 21:51:56 +0530

    update the initial project

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
          This is a workshop about how to create a react app using github
+         Git is a technology and one of the company that provide this technology is called github
         </p>
         <a
           className="App-link"
